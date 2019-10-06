5e7f7c7c07d53c2603b286bbb460577d7e2cf5f6
David Guy Brizan <dbrizan@gmail.com>
2019-10-06 09:12:43 -0700

    "Add to container" functions throw exceptions.

--- Queue.java
+++ Queue.java
@@ -1,7 +1,7 @@
 
 public interface Queue<T> {
 	
-	public T dequeue();
+	public T dequeue() throws Exception;
 	public void enqueue(T item);
 	public boolean empty();
 
--- Stack.java
+++ Stack.java
@@ -2,8 +2,8 @@
 public interface Stack<T> {
 	
 	public void push(T item);
-	public T pop();
-	public T peek();
+	public T pop() throws Exception;
+	public T peek() throws Exception;
 	public boolean empty();
 
 }
